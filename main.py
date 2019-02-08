d02fac1fe85402800f1a2883562d1daeb429cbb7
S. Stewart <rubix.this@gmail.com>
2019-02-08 16:53:27 -0700

    Final commit before submit

--- main.py
+++ main.py
@@ -71,74 +71,74 @@ class Polynomial:
         """
 
         This function will take two polynomial coefficients and then multiply them
-        together as if it were to simulate a
+        together as if it were to simulate the distributive property.
+
+        Coefficient representation is as follows:
+
+        c*x^0 + c*x^1 + c*x^2 + ... + c*x^n
 
         """
+
+        # Index's to help us interact through double nested for loop.
         outerLoopIndex = 0
         innerLoopIndex = 0
 
-        isNotPopped = True
+        # Special exception for when first run through for OuterLoopIndex = 0
+        needToPopFirstZero = True
 
+        # Polynomials to hold our values between for loop iterations
         tempPolynomial = Polynomial([])
         bigPolynomial = Polynomial([])
 
+        # After each iteration of this loop, it will add 2 coefficient segments together
         for x in range(len(self)):
+
+            # Each loop of this will times two integers together, and then add them to a temp
+            # polynomial.
             for y in range(len(rhs)):
 
-                print("OuterLoop Index: ", outerLoopIndex)
-                print("InnerLoop Index: ", innerLoopIndex)
-
-                print("self coeff", self.coefficients)
-                print("rhs coeff", rhs.coefficients)
-
-                print("AddZero function is: ", addZero(outerLoopIndex))
-                print("tempPolynomial.coefficients: ", tempPolynomial.coefficients)
-                print("AddZero and tempPoly coeff: ", (addZero(outerLoopIndex) + tempPolynomial.coefficients))
-                print("self:", self.coefficients[outerLoopIndex])
-                print("rhs:", rhs.coefficients[innerLoopIndex])
-
-                print("self * rhs:", [self.coefficients[outerLoopIndex] * rhs.coefficients[innerLoopIndex]])
-
+                # Take one coefficient from the left side, and times it against one on the right side.
                 tempPolynomial.coefficients = tempPolynomial.coefficients + ([self.coefficients[outerLoopIndex] * rhs.coefficients[innerLoopIndex]])
 
-                print("tempPolynomial.coefficients: ", tempPolynomial.coefficients)
+                # Special exception for the first run through, getting ride of zero.
+                if needToPopFirstZero:
 
-                if isNotPopped:
+                    # Pop the first zero because we don't want it yet. First
+                    # time through doesn't need a zero to offset the polynomial
                     tempPolynomial.coefficients = [tempPolynomial.coefficients.pop()]
-                    isNotPopped = False
+                    needToPopFirstZero = False
 
-                    print("if statement was called")
-
-                print("tempPolynomial.coefficients: ", tempPolynomial.coefficients)
-
-
-                print("")
-                print("")
-                print("")
-                print("")
+                # Iterate the value for the next loop.
                 innerLoopIndex = innerLoopIndex + 1
 
+            # Save the end result here after each loop by adding two polynomials
+            # together.
             bigPolynomial.coefficients = (bigPolynomial + tempPolynomial).coefficients
-            print("This is big list: ", bigPolynomial.coefficients)
 
+            # Call recursive function to pad the from of the polynomial as many
+            # times as we need to, to create a viable adding between two
+            # coefficients
             tempPolynomial.coefficients = addZero(outerLoopIndex)
+
+            # Increase outerloop index, and reset inner loop.
             outerLoopIndex = outerLoopIndex + 1
             innerLoopIndex = 0
 
-
-        print("This is big list end: ")
-        print(bigPolynomial.coefficients)
-        print(tempPolynomial.coefficients)
-
-
-        print(bigPolynomial.coefficients)
-
+        # return the final option.
         return Polynomial(bigPolynomial.coefficients)
 
     def __len__(self):
 
+        """
+
+        This will return the length of the polynomial.
+        """
+
         return len(self.coefficients)
 
+
+# Recursive function to add padding to the front of a polynomial
+# to help with the addition process. Only for Mult function.
 def addZero(numOfIterations):
 
     if numOfIterations+1 == 1:
@@ -146,6 +146,8 @@ def addZero(numOfIterations):
     elif numOfIterations+1 > 1:
         return [0] + addZero(numOfIterations - 1)
 
+
+# Test cases for len function
 def test_len():
 
     p1 = Polynomial([2])
@@ -155,15 +157,20 @@ def test_len():
     assert len(p2) == 2
 
 
+# Test cases for the call function. If we evaluate xm
+# with a simple cx^0 + cx^1 + cx^2 + ... + cx^n notation
 def test_call():
 
     p1 = Polynomial([2])
     p2 = Polynomial([3, 2])
+    p7 = Polynomial([1, 5, 8, 2, 34])
 
     assert p1(x=5) == 2
     assert p2(x=5) == 13
+    assert p7(x=4) == 8981
 
 
+# Test cases for mult function
 def test_mult():
 
     p1 = Polynomial([2])
@@ -183,6 +190,8 @@ def test_mult():
     assert (p1 * p2 * p3).coefficients == [30]
     assert (p1 * p6 * p7).coefficients == [10, 64, 150, 132, 368, 476]
 
+
+# Test cases for add function
 def test_add0():
 
     p1 = Polynomial([2])
@@ -212,7 +221,7 @@ def test_add0():
     assert (p6 + p7 + p2).coefficients == [9, 12, 8, 2, 34]
 
 
-
+# A main function that doesn't really serve a purpose
 if __name__ == '__main__':
     p = Polynomial([1, 5, 6])
     p2 = p + p
